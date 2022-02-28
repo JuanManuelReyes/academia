79b98a3c60b9db35cf05d6e5654f1be6a6bdc863
Juan Reyes <juanmanuelreyesarrambide@gmail.com>
2022-02-28 12:09:19 -0300

    Test 1 Terminado

--- js/question1.js
+++ js/question1.js
@@ -1,154 +1,106 @@
 //Array of Objects
+//img:'../media/img/stop.png'
 const quiz = [
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-        img:'../media/img/stop.png'
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
+        q:'Principio de seguridad vial: los usuarios de las vías de tránsito deben abstenerse de todo acto que pueda constituir un peligro o un obstáculo para la circulación, poner en peligro a personas o, causar daños a bienes públicos o privados.',
+        options: ['Verdadero','Falso'],
         answer: 0
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
+        q:'¿Qué indica esta señal?',
+        options: ['Próximamente aparecerá tránsito sobre la izquierda.','Próximamente curva a la derecha.','Ninguna es correcta.','Próximamente curva a la izquierda.'],
+        answer: 1,
+        img:'../media/testIMG/CurvaDerecha.png'
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
+        q:'Está terminantemente prohibido estacionar...',
+        options: ['En la acera derecha de las calles de un solo sentido.','En la puerta de una escuela.','En la mitad de la cuadra.','A menos de 10 metros de una señal de PARE o CEDA EL PASO.'],
+        answer: 3
+    },
+    {
+        q:'Si su vehículo comienza a patinar al momento de frenar...',
+        options: ['Acelere.','Frene enérgicamente.','Todas son incorrectas.','Procure detenerlo bombeando intermitentemente el freno.'],
+        answer: 3
+    },
+    {
+        q:'El conductor de un vehículo puede sobrepasar a otro por la derecha cuando...',
+        options: ['Nunca.','El vehículo de adelante va a doblar a la derecha.','El veículo de adelante va a doblar a la izquierda.','El vehículo de adelante da paso a otros.'],
+        answer: 2
+    },
+    {
+        q:'La mayor parte de los accidentes de tránsito por desperfectos del vehículo, a excepción de los frenos, son producidos por:',
+        options: ['El hidroplaneamiento.','Problemas de suspención.','Los neumaticos gastados.'],
+        answer: 2,
+    },
+    {
+        q:'A llegar a un cruce con su vehículo Ud. tiene luz verde al frente y una ambulancia haciendo uso de la sirena y luces intermitentes, aparece por la izquierda pidiendo paso, intentando cruzar con luz roja al frente, que debe hacer Ud.?',
+        options: ['Detenerse y comenzar a tocar bocina encendiendo todas las luces del vehículo para hacerse eco de la emergencia y avisar a los demás usuarios.','Proseguir la marcha porque usted tiene luz verde al frente y la ambulancia está en infracción al cruzar con luz roja.','No cruzar y dar preferencia de paso a la ambulancia.'],
+        answer: 2
+    },
+    {
+        q:'Para señalar con el brazo la disminución de velocidad o detención del vehículo, se deberá colocar:',
+        options: ['En ángulo recto hacia arriba.','Extendido horizontalmente.','Ninguna es correcta.','En ángulo recto hacia abajo.'],
+        answer: 3,
+    },
+    {
+        q:'Si observa que enciende las luces blancas traseras de un vehículo ellos significa:',
+        options: ['Que da marcha atrás.','Que está frenando.','Que se dispone a pasar otro coche.','Que va a girar a la derecha.'],
         answer: 0
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
+        q:'Cuando los neumáticos tienen poca presión de aire, se ahorra combustible:',
+        options: ['Verdadero.','Falso.'],
+        answer: 1
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
+        q:'En una bajada pronunciada para mayor seguridad debe poner el vehículo en punto muerto:',
+        options: ['Verdadero.','Falso.'],
+        answer: 1
+    },
+    {
+        q:'La siguiente señal advierte:',
+        options: ['Camino sinuoso.','No conducir en estado etílico.','Pavimento resbaladizo.'],
+        answer: 2,
+        img:'../media/testIMG/CaminoResbaladizo.png'
+    },
+    {
+        q:'Ante estas circunstancias: lluvia, niebla, humo, atardecer. ¿Con qué luces deberá circular?',
+        options: ['Luces de posición.','Luz de marcha atrás.','Luces de largo alcance.','Luces de corto alcance.'],
+        answer: 3
+    },
+    {
+        q:'Usted puede usar la bocina de su vehículo para...',
+        options: ['Prevenir un accidente en caso de extrema necesidad.','Apurar al que va adelante.','Avisar que va a estacionar.','Siempre, salvo cuando pasa frente a un hospital.'],
         answer: 0
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
+        q:'En un cruce no regulado,¿qué vehículo tiene el derecho preferente de paso?',
+        options: ['El que aparece por la derecha.','El que aparece por la izquierda.','El que llegue último al cruce.','El que llegue primero al cruce.'],
         answer: 0
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
+        q:'Las luces altas ¿deben sustituirse por las de alcance medio cuando hay riesgo de encandilamiento?',
+        options: ['Si.','No.'],
         answer: 0
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
+        q:'Cuando usted dobla a la derecha:',
+        options: ['Debe encender el señalero derecho.','Debe mirar por el espejo retrovisor, encender el señalero y acercarse al cordón derecho.','Debe acercar su vehículo a la izquierda, encender el señalero y doblar con precaución.'],
+        answer: 1
+    },
+    {
+        q:'La IMM está capacitada para retirar de la vía pública los vehículos que están mal estacionados o en lugares prohibidos.',
+        options: ['Verdadero.','Falso.'],
         answer: 0,
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
+        q:'La velocidad ideal para conducir con seguridad en carretera está entre:',
+        options: ['100-120 km/hora.','50-60 km/hora.','70-90 km/hora.','Ninguna es correcta.'],
+        answer: 2
     },
     {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0
-    },
-    {
-        q:'Pregunta',
-        options: ['a','b','c','d'],
-        answer: 0,    
+        q:'Un vehículo que circula a 65 km/hora. necesita para detenerse...',
+        options: ['2 metros.','10 metros.','25 metros.','Media Cuadra.'],
+        answer: 3
     }
 ]
